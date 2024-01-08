9f19786e1e88735513840f546151ede790844504
tsogbadrakh baatar <23LP4804@iMAC2021-05s-iMac.local>
2024-01-08 11:52:32 +0800

    project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Card from "./components/Card";
 export default function App (){
     return (
         <div>
-            <p>Hello World</p>git
+            <p>Hello World</p>
+            <Card/>
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmiro.medium.com%2Fv2%2Fresize%3Afit%3A1358%2F1*sMixuj43WhAjtwmOWR48QA.jpeg&tbnid=XHJQgKxqNwKWmM&vet=12ahUKEwirwrDf8cyDAxUwha8BHU4ZCQUQMygNegUIARCOAQ..i&imgrefurl=https%3A%2F%2Fmedium.com%2F%40amelia.c._writer_94790%2Fakita-shiba-inu-541084f6923&docid=lWgGFC0A7_BQjM&w=1280&h=960&q=akita&ved=2ahUKEwirwrDf8cyDAxUwha8BHU4ZCQUQMygNegUIARCOAQ" alt="" />
         </div>
     );
 }
